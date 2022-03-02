4368803aef3f4e5c8172b95c5d17b609aa87d53d
董建成 <1014236121@qq.com>
2022-03-02 21:59:01 +0800

    两两交换链表中的节点

---
+++ 微软CodeTop/024_两两交换链表中的节点/Solution.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+  let dummy = new ListNode(0)
+  dummy.next = head
+  let temp = dummy
+  while (temp.next !== null && temp.next.next !== null) {
+    const node1 = temp.next
+    const node2 = node1.next
+    temp.next = node2
+    node1.next = node2.next
+    node2.next = node1
+    temp = node1
+  }
+  return dummy.next
+};
