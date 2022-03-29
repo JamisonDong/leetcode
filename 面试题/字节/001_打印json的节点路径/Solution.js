1597271bbd35a28eac4c09f63aaf6a0d81c38ea2
董建成 <1014236121@qq.com>
2022-03-29 16:05:49 +0800

    打印json的节点路径

--- 面试题/字节/001_打印json的节点路径/Solution.js
+++ 面试题/字节/001_打印json的节点路径/Solution.js
@@ -13,20 +13,34 @@
 // ["a.list.b.b1", "a.list.c.c1", "d.e.e1"]
 
 
-// const getRes = (json_str) => {
-//   let res = []
-//   let path = []
-//   function getPath(keys) {
+const getRes = (json_str) => {
+  let res = []
+  let path = []
+  function backtracking(obj) {
 
-//     if()
+    for (let i of Object.keys(obj)) {
+      path.push(i + ".")
+      if (Array.isArray(obj[i])) {
+        path.push('list.')
+        for (let j of obj[i]) {
+          // path.push(".")
+          backtracking(j)
+          path.pop()
+        }
+        path.pop()
+      } else if (typeof obj[i] === 'object') {
+        backtracking(obj[i])
+      } else {
+        path.push(obj[i])
+        res.push([...path].join(""))
+      }
+      path.pop()
+    }
+  }
 
-//     for (let i of Object.keys(json_str)) {
-//       if (Array.isArray(json_str[i])) {
-
-//       }
-//     }
-//   }
-// }
+  backtracking(json_str)
+  return res
+}
 
 const json_str = {
   "a": [
@@ -37,6 +51,7 @@ const json_str = {
     "e": "e1"
   }
 }
-["a.list.b.b1", "a.list.c.c1", "d.e.e1"]
+console.log(getRes(json_str));
+// ["a.list.b.b1", "a.list.c.c1", "d.e.e1"]
 
 
