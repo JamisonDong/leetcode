e280ccb0fcf17b0f3761080f05cf76d89365d019
董建成 <1014236121@qq.com>
2022-03-30 16:30:03 +0800

    修正setTimeout不准

--- 面试题/修正setTimeout不准.js
+++ 面试题/修正setTimeout不准.js
@@ -1,9 +1,5 @@
 let startTime = new Date().getTime()
 let count = 0
-// setInterval(function () {
-//   let i = 0
-//   while (i++ < 10000000);
-// }, 0)
 
 function fixed() {
   count++
@@ -15,4 +11,42 @@ function fixed() {
   console.log(new Date().getTime() - (startTime + count * 1000));
 }
 
-setTimeout(fixed, 1000)
+// setTimeout(fixed, 1000)
+
+
+
+const Solution = function () {
+  let startTime, // 开始时间
+    count, // 计数器
+    runTime, // 当前时间
+    downSecond = 1200,  // 倒计时时间
+    loopTimer = null;
+
+  // 重置
+  function resetDefaultValue() {
+    startTime = new Date().getTime()
+    count = 0
+    runTime = 0
+  }
+
+  function loop() {
+    runTime = Date.now();
+    let offsetTime = runTime - (startTime + count * 1000); //时间差
+    count++;
+    let nextTime = 1000 - offsetTime; //下一次定时器需要的时间
+    nextTime = nextTime > 0 ? nextTime : 0;
+    downSecond--;
+    console.log('时间差：' + offsetTime, '，下一次需要时间：' + nextTime)
+    if (downSecond <= 0) {
+      // 结束定时器
+      clearTimeout(loopTimer)
+      loopTimer = null;
+      return false;
+    }
+    loopTimer = setTimeout(loop, nextTime);
+  }
+  resetDefaultValue()
+  loop()
+}
+
+Solution()
