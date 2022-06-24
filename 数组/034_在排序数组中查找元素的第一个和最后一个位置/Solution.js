47b1ec834a6cead488850e8338adb859b8c4229f
Jamison Dong <t-jdong@microsoft.com>
2022-06-24 15:15:34 +0800

    二分

--- 数组/034_在排序数组中查找元素的第一个和最后一个位置/Solution.js
+++ 数组/034_在排序数组中查找元素的第一个和最后一个位置/Solution.js
@@ -1,34 +1,74 @@
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// var searchRange = function (nums, target) {
+//   function getIndex(nums, target, lower) {
+//     const len = nums.length
+//     let left = 0, right = len - 1, res = len
+//     while (left <= right) {
+//       let mid = Math.floor(left + (right - left) / 2)
+//       if (nums[mid] > target || (lower && nums[mid] >= target)) {
+//         right = mid - 1
+//         res = mid
+//       } else {
+//         left = mid + 1
+//       }
+//     }
+//     return res
+//   }
+
+//   let ans = [-1, -1]
+//   // 第一个大于等于target的元素下标
+//   const leftIndex = getIndex(nums, target, true)
+//   // 第一个大于target的元素下标
+//   const rightIndex = getIndex(nums, target, false) - 1
+//   if (leftIndex <= rightIndex && rightIndex < nums.length && nums[leftIndex] === target && nums[rightIndex] === target) {
+//     ans = [leftIndex, rightIndex]
+//   }
+//   return ans
+// };
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var searchRange = function (nums, target) {
-  function getIndex(nums, target, lower) {
-    const len = nums.length
-    let left = 0, right = len - 1, res = len
-    while (left <= right) {
-      let mid = Math.floor(left + (right - left) / 2)
-      if (nums[mid] > target || (lower && nums[mid] >= target)) {
-        right = mid - 1
-        res = mid
-      } else {
-        left = mid + 1
-      }
+    const len = nums.length;
+    function getIndex(lower) {
+        let left = 0,
+            right = len - 1,
+            res = len;
+        while (left <= right) {
+            let mid = Math.floor(left + (right - left) / 2);
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                res = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
     }
-    return res
-  }
 
-  let ans = [-1, -1]
-  // 第一个大于等于target的元素下标
-  const leftIndex = getIndex(nums, target, true)
-  // 第一个大于target的元素下标
-  const rightIndex = getIndex(nums, target, false) - 1
-  if (leftIndex <= rightIndex && rightIndex < nums.length && nums[leftIndex] === target && nums[rightIndex] === target) {
-    ans = [leftIndex, rightIndex]
-  }
-  return ans
+    let res = [-1, -1];
+    let left = getIndex(true);
+    let right = getIndex(false) - 1;
+    console.log(left, right);
+    if (
+        left <= right &&
+        right < len &&
+        nums[left] === target &&
+        nums[right] === target
+    ) {
+        res = [left, right];
+    }
+
+    return res;
 };
 
-const nums = [1], target = 1
+const nums = [5, 7, 7, 8, 8, 10],
+    target = 8;
 console.log(searchRange(nums, target));
---
+++ 数组/704_二分查找/Solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let len = nums.length;
+    let left = 0,
+        right = len - 1;
+    while (left < right) {
+        let middle = Math.floor(left + (right - left) / 2);
+        if (nums[middle] === target) {
+            return middle
+        }
+        else if (nums[middle] > target) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+    return -1;
+};
+
+const nums = [-1, 0, 3, 5, 9, 12];
+const target = 9;
+console.log(search(nums, target));
